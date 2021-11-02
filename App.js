44a2e37f708965f4e293d5b49291e4c3e44db412
Bello Babakolo <fleetbeekay@gmail.com>
2021-11-03 00:24:09 +0100

    Display different images on the screen

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, TouchableOpacity, Image, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, Image, FlatList } from 'react-native';
 
 export default function App() {
-  const uri = 'https://picsum.photos/1000/1000';
+
+  const imagesDataList = [];
+  for (let index=0; index < 10; index++) {
+    imagesDataList.push({ 'uri': `https://picsum.photos/200/300?random=${index}`, key: `index-${index}` })
+  }
 
   const onImagePress = () => alert("You pressed an Image");
 
-  const ClickableImage = ({ height, width, onPress }) => (
+  const ClickableImage = ({ height, width, onPress, uri }) => (
     <TouchableOpacity
       onPress={onPress}
       style={{ height, width }}
@@ -22,10 +26,13 @@ export default function App() {
   );
 
   return (
-    <View style={styles.container}>
-      <ClickableImage width={'100%'} height={200} onPress={onImagePress} />
-    </View>
-  );
+    <FlatList
+      data={imagesDataList}
+      renderItem={({ item, index, separators }) => (
+        <ClickableImage width={'100%'} height={200} onPress={onImagePress} uri={item.uri} />
+      )}
+    />
+  )
 }
 
 const styles = StyleSheet.create({
