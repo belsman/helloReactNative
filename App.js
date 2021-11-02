274ca3be112f466976970319c087221a88c985ed
Bello Babakolo <fleetbeekay@gmail.com>
2021-11-02 23:42:08 +0100

    Add clicklistner to the image

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, Image, View } from 'react-native';
 
 export default function App() {
+  const uri = 'https://picsum.photos/1000/1000';
+
+  const onImagePress = () => alert("You pressed an Image");
+
+  const ClickableImage = ({ height, width, onPress }) => (
+    <TouchableOpacity
+      onPress={onPress}
+      style={{ height, width }}
+    >
+      <Image 
+        source={{
+          uri,
+        }}
+        style={styles.image}
+      />
+    </TouchableOpacity>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ClickableImage width={'100%'} height={200} onPress={onImagePress} />
     </View>
   );
 }
@@ -18,4 +35,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: '100%',
+    height: '100%'
+  }
 });
